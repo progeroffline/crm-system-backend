c9c3278ff10b01c36c0c96f8f88586bf934e15ae
pablo <progeroffline@gmail.com>
2025-08-15 13:48:38 +0300

    Increase version and add initals

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { RequestLoggerMiddleware } from './common/middlewares/request.logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  
 
   const config = new DocumentBuilder()
     .setTitle('CRM System API')
